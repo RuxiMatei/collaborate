30ce46d9c9dd2130545e7e10faeb455f3c4b7c90
Ruxandra Matei <Ruxandra.Matei@gfk-cps.com>
2024-10-04 15:02:27 +0300

    add projects

---
+++ collaborate-be/src/test/java/org/rmid/collaborate/CollaborateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rmid.collaborate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollaborateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
